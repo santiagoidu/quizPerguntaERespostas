b24fa49b2cabd2488cf33884fcb2f589308ddb6e
Marco Tulio Santiago <48780041+santiagoidu@users.noreply.github.com>
2021-09-05 17:12:02 -0300

    Configurando e exibindo HTML com EJS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//ejs
+const express = require("express")
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) =>{
+    res.render(
+        "index")
+})
+
+app.listen(8080, () => { console.log("App rodando!!!")})

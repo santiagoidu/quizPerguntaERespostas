d8e9bdfa53ed3aefa8e34ac7bb5953b45a15e594
Marco Tulio Santiago <48780041+santiagoidu@users.noreply.github.com>
2021-09-07 13:23:23 -0300

    Capturando dados do formulário

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// rotas
 app.get("/", (req, res) =>{
     res.render("index")
 })
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send(`Formulario recebido titulo: ${titulo}, descricao ${descricao}`)
 })
 
 app.listen(8080, () => { console.log("App rodando!!!")})

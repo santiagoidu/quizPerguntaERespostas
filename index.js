cb7276fab304a5284142eea93428d03d52856806
Marco Tulio Santiago <48780041+santiagoidu@users.noreply.github.com>
2021-09-06 21:18:03 -0300

    Inicio do projeto EJS

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express")
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get("/", (req, res) =>{
-    res.render(
-        "index")
+    res.render("index")
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
 })
 
 app.listen(8080, () => { console.log("App rodando!!!")})
